02635f1e0d225c897ce4c0c8bd6c2a2122e8f5a0
Raviteja Manepalli <raviteja.manepalli@geidea.net>
2024-07-03 08:27:29 +0300

    added health check, startup and entrypoint scripts

---
+++ service1/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+# Perform some initialization tasks if needed
+
+ echo "This is entrypoint"
+exec "$@"
--- service1/startup.sh
+++ service1/startup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+set -e
 
-# Start script for your Spring Boot application
+# # Example: Wait for a database to be ready
+# until nc -z -v -w3 db 3306
+# do
+#   echo "Waiting for database connection..."
+#   sleep 1
+# done
 
-echo "Starting Spring Boot application..."
-exec ./mvnw spring-boot:run
+echo "Waiting for some connection..."
+
+# Start the main process
+exec java -jar app.jar
